87b82eaac4ed1fa4810165d3744239e53ab3fca4
JK3WN <yimjay1@gmail.com>
2024-10-01 12:49:30 +0900

    Door Remade

--- Assets/YJK/DoorKick.cs
+++ Assets/YJK/DoorKick.cs
@@ -5,21 +5,76 @@ using UnityEngine;
 // Made By JK3WN
 public class DoorKick : MonoBehaviour
 {
+    #region References
+    [SerializeField] Transform _left;
+    [SerializeField] Transform _right;
+    #endregion
+    [SerializeField] float _kickSpeed = 10f;
     private AudioSource _as;
+    private Collider2D _collider;
     private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
+    bool _destroyable = true;
 
     private void Start()
     {
         if(GetComponent<Rigidbody2D>() != null) _rb = GetComponent<Rigidbody2D>();
         if(GetComponent<AudioSource>() != null) _as = GetComponent<AudioSource>();
+        if(GetComponent<SpriteRenderer>() != null) _sr = GetComponent<SpriteRenderer>();
+        if(GetComponent<Collider2D>() != null) _collider = GetComponent<Collider2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        /*
         if (_rb.angularVelocity > 100 || _rb.angularVelocity < -100)
         {
             _as.Play();
             if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().Spawn_Wave();
         }
+        */
+        if (!(collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Player"))) return;
+        _rb.bodyType = RigidbodyType2D.Kinematic;
+        GetComponent<BoxCollider2D>().size = new Vector2(0.8f, 1f);
+        GetComponent<Collider2D>().isTrigger = true;
+        _as.Play();
+        if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().Spawn_Wave();
+        if (Vector2.Distance(_left.position, collision.transform.position) < Vector2.Distance(_right.position, collision.transform.position))
+        {
+            _rb.velocity = (this.transform.position - _left.position) * _kickSpeed;
+        }
+        else
+        {
+            _rb.velocity = (this.transform.position - _right.position) * _kickSpeed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<EnemyHealth>().GetDamaged(collision.transform.position - this.transform.position);
+            if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().Spawn_Wave();
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().GetDamaged(collision.transform.position - this.transform.position);
+            if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().Spawn_Wave();
+            _sr.enabled = false;
+            _collider.enabled = false;
+            DelayedDestroy();
+        }
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().Spawn_Wave();
+            _sr.enabled = false;
+            _collider.enabled = false;
+            DelayedDestroy();
+        }
+    }
+    IEnumerator DelayedDestroy()
+    {
+        yield return new WaitForSeconds(15f);
+        Destroy(gameObject);
     }
 }
