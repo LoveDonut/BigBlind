baa8deaf45418d550ecbc4d922df2fb65a598761
jinhachun <jinhachun@gmail.com>
2024-10-07 18:54:22 +0900

    [feat] 캐릭터 붕쯔붕쯔 스프라이트

---
+++ Assets/JHC/Script/PlayerRotate.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    [Header("Reference")]
+    [SerializeField] Transform _spriteObj;
+
+    CameraTarget _cameraTarget;
+    
+    void Awake()
+    {
+        _cameraTarget = FindAnyObjectByType<CameraTarget>();
+    }
+
+    void Update()
+    {
+        Rotate();
+    
+    }
+
+    void Rotate()
+    {
+        Vector3 aimPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(aimPos.y, aimPos.x) * Mathf.Rad2Deg - 270f ;
+        _spriteObj.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
