19581ee863a7713ea9c55fc800b2b1f7f836772f
JK3WN <yimjay1@gmail.com>
2024-10-09 15:12:50 +0900

    Made Variables VIsible In Inspector

--- Assets/Scripts/Player/PlayerShortAttack.cs
+++ Assets/Scripts/Player/PlayerShortAttack.cs
@@ -26,6 +26,8 @@ public class PlayerShortAttack : MonoBehaviour
 
     [SerializeField] float _slowDownDuration = 2f;
     [SerializeField] float _slowDownOffset = 0.2f;
+    [SerializeField] float _zoomSizeFactor = 0.8f;
+    [SerializeField] float _startZoomDuration = 0.01f;
 
     Coroutine _attackCoroutine;
     Animator _animator;
@@ -102,7 +104,7 @@ public class PlayerShortAttack : MonoBehaviour
         {
             hit.GetComponent<DoorKick>().DoorKicked(transform);
             TimeManager.Instance.DoSlowMotion(_slowDownOffset, _slowDownDuration);
-            GameObject.Find("Virtual Camera").GetComponent<CameraZoom>().ZoomCamera(0.8f, 0.01f, 0f, _slowDownDuration);
+            GameObject.Find("Virtual Camera").GetComponent<CameraZoom>().ZoomCamera(_zoomSizeFactor, _startZoomDuration, 0f, _slowDownDuration);
         }
     }
 
