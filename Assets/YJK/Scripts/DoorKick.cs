2aa9489474b97fa930e89238d125aa9455d3fca6
JK3WN <yimjay1@gmail.com>
2024-10-16 15:45:03 +0900

    Refactored CameraTarget & DoorKick

--- Assets/YJK/Scripts/DoorKick.cs
+++ Assets/YJK/Scripts/DoorKick.cs
@@ -9,27 +9,60 @@ public class DoorKick : MonoBehaviour
     #region References
     [SerializeField] Transform _left;
     [SerializeField] Transform _right;
-    #endregion
-    [SerializeField] float _kickSpeed = 10f;
-    private AudioSource _as;
-    private Collider2D _collider;
-    private Rigidbody2D _rb;
-    private SpriteRenderer _sr;
-    private bool _isMoving = false;
 
     [Header("SFX")]
     [SerializeField] AudioClip _smashDoor;
     [SerializeField] AudioClip _enemyBurst;
     [SerializeField] AudioClip _crashDoor;
+    #endregion
 
+    #region PrivateVariables
+    [SerializeField] float _kickSpeed = 10f;
+    private AudioSource _as;
+    private Collider2D _collider;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
+    private bool _isMoving;
+    #endregion
+
+    #region PrivateMethods
     private void Start()
     {
         if(GetComponent<Rigidbody2D>() != null) _rb = GetComponent<Rigidbody2D>();
         if(GetComponent<AudioSource>() != null) _as = GetComponent<AudioSource>();
         if(GetComponent<SpriteRenderer>() != null) _sr = GetComponent<SpriteRenderer>();
         if(GetComponent<Collider2D>() != null) _collider = GetComponent<Collider2D>();
+        _isMoving = false;
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!_isMoving) return;
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            _as.PlayOneShot(_enemyBurst);
+            collision.gameObject.GetComponent<EnemyHealth>().GetDamaged(collision.transform.position - this.transform.position);
+            if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().SpawnWave();
+        }
+        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Obstacle"))
+        {
+            _as.PlayOneShot(_crashDoor);
+            _sr.enabled = false;
+            _collider.enabled = false;
+            _rb.bodyType = RigidbodyType2D.Static;
+            DelayedDestroy();
+            if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().SpawnWave();
+        }
+    }
+    
+    IEnumerator DelayedDestroy()
+    {
+        yield return new WaitForSeconds(_crashDoor.length * 2);
+        Destroy(gameObject);
+    }
+    #endregion
+
+    #region PublicMethods
     public void DoorKicked(Transform kicker)
     {
         _rb.bodyType = RigidbodyType2D.Kinematic;
@@ -48,67 +81,5 @@ public class DoorKick : MonoBehaviour
         }
         _isMoving = true;
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (!_isMoving) return;
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            _as.PlayOneShot(_enemyBurst);
-            collision.gameObject.GetComponent<EnemyHealth>().GetDamaged(collision.transform.position - this.transform.position);
-            if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().SpawnWave();
-        }
-        /*
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            collision.gameObject.GetComponent<PlayerHealth>().GetDamaged(collision.transform.position - this.transform.position);
-            if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().SpawnWave();
-            _sr.enabled = false;
-            _collider.enabled = false;
-            DelayedDestroy();
-        }
-        */
-        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Obstacle"))
-        {
-            _as.PlayOneShot(_crashDoor);
-            _sr.enabled = false;
-            _collider.enabled = false;
-            DelayedDestroy();
-            if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().SpawnWave();
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if(!_isMoving) return;
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            _as.PlayOneShot(_enemyBurst);
-            collision.gameObject.GetComponent<EnemyHealth>().GetDamaged(collision.transform.position - this.transform.position);
-            if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().SpawnWave();
-        }
-        /*
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            collision.gameObject.GetComponent<PlayerHealth>().GetDamaged(collision.transform.position - this.transform.position);
-            if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().SpawnWave();
-            _sr.enabled = false;
-            _collider.enabled = false;
-            DelayedDestroy();
-        }
-        */
-        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Obstacle"))
-        {
-            _as.PlayOneShot(_crashDoor);
-            _sr.enabled = false;
-            _collider.enabled = false;
-            DelayedDestroy();
-            if (GetComponent<WaveManager>() != null) GetComponent<WaveManager>().SpawnWave();
-        }
-    }
-    IEnumerator DelayedDestroy()
-    {
-        yield return new WaitForSeconds(_crashDoor.length * 2);
-        Destroy(gameObject);
-    }
+    #endregion
 }
